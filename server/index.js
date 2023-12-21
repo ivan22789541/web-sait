07ce1d251b95cf82a2320a1e46aa6ef42edcfd6e
ergennn <ivan554854@mail.ru>
2023-12-21 21:00:15 +0500

    serv

--- server/index.js
+++ server/index.js
@@ -10,9 +10,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'WORKING!!!'})
-})
 
 const start = async () => {
     try {
