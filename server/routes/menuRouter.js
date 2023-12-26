dca048dfebc82da73caed700b18d90e4459394ff
ergennn <ivan554854@mail.ru>
2023-12-26 23:49:29 +0500

    server

--- server/routes/menuRouter.js
+++ server/routes/menuRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const menuController = require('../controllers/menuController')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
 
-router.post('/', menuController.create)
+router.post('/', checkRole('ADMIN'), menuController.create)
 router.get('/', menuController.getAll)
 router.get('/:id', menuController.getOne)
 
